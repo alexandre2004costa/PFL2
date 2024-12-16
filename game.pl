407dad4c99ab72d52ea9831ccebb5595ad642d41
Sofia Gonçalves <132204152+SofiaX5@users.noreply.github.com>
2024-12-16 18:15:50 +0000

    Update to sictus

--- game.pl
+++ game.pl
@@ -2,6 +2,9 @@
 print_n(0,S):-!.
 print_n(N,S):- N1 is N-1, print_n(N1, S), put_char(S).
 
+print_n_code(0,S):-!.
+print_n_code(N,S):- N1 is N-1, print_n_code(N1, S), put_code(S).
+
 line(0,S,P):-!.
 line(X,S,P):- X1 is X-1, put_char(S), print_n(P-2,' '), put_char(S), nl,  line(X1,S,P).
 
@@ -73,27 +76,27 @@ transition(mode, 4, initial).
 transition(_, _, initial). 
 
 board([
-    ['█', '█', '█', '█', '█', '█', '█', '█', '█', '█', '█', '█', '█', '█', '█', '█', '█', '█', '█', '█'],
-    ['█', '█', '█', '█', '█', '█', '█', '█', '█', '█', '█', '█', '█', '█', '█', '█', '█', '█', '█', '█'],
-    ['█', '█', '█', '█', '█', '█', '█', '█', '█', '█', '█', '█', '█', '█', '█', '█', '█', '█', '█', '█'],
-    ['█', '█', '█', '█', '█', '█', '█', '█', '█', '█', '█', '█', '█', '█', '█', '█', '█', '█', '█', '█'],
-    ['█', '█', '█', '█', '█', '█', '█', '█', '▓', '▓', '▒', '▒', '█', '█', '█', '█', '█', '█', '█', '█'],
-    ['█', '█', '█', '█', '█', '█', '█', '█', '▒', '▒', '▓', '▓', '█', '█', '█', '█', '█', '█', '█', '█'],
-    ['█', '█', '█', '█', '█', '█', '█', '█', '█', '█', '█', '█', '█', '█', '█', '█', '█', '█', '█', '█'],
-    ['█', '█', '█', '█', '█', '█', '█', '█', '█', '█', '█', '█', '█', '█', '█', '█', '█', '█', '█', '█'],
-    ['█', '█', '█', '█', '█', '█', '█', '█', '█', '█', '█', '█', '█', '█', '█', '█', '█', '█', '█', '█'],
-    ['█', '█', '█', '█', '█', '█', '█', '█', '█', '█', '█', '█', '█', '█', '█', '█', '█', '█', '█', '█']
+    ['S', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S'],
+    ['S', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S'],
+    ['S', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S'],
+    ['S', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S'],
+    ['S', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'W', 'W', 'B', 'B', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S'],
+    ['S', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'B', 'B', 'W', 'W', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S'],
+    ['S', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S'],
+    ['S', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S'],
+    ['S', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S'],
+    ['S', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S']
 ]).
 
 
 display_board([]) :- !.
 display_board([Row | Rest]) :-
     length(Row, Length), Len is Length + 6,
-    put_char('┌'), print_n(Len, '─'), put_char('┐'), nl,
-    put_char('│'), write(' '), print_n(2, '█'), print_n(Length, '▓'), print_n(2, '█'), write(' '), put_char('│'), nl,
+    put_code(0x250C), print_n_code(Len, 0x2500), put_code(0x2510), nl,
+    put_code(0x2502), write(' '), print_n_code(2, 0x2588), print_n_code(Length, 0x2593), print_n_code(2, 0x2588), write(' '), put_code(0x2502), nl,
     display_board_rows([Row | Rest]),
-    put_char('│'), write(' '), print_n(2, '█'), print_n(Length, '▓'), print_n(2, '█'), write(' '), put_char('│'), nl,
-    put_char('└'), print_n(Len, '─'), put_char('┘'), nl.
+    put_code(0x2502), write(' '), print_n_code(2, 0x2588), print_n_code(Length, 0x2593), print_n_code(2, 0x2588), write(' '), put_code(0x2502), nl,
+    put_code(0x2514), print_n_code(Len, 0x2500), put_code(0x2518), nl.
 
 display_board_rows([]) :- !.
 display_board_rows([Row | Rest]) :-
@@ -101,14 +104,19 @@ display_board_rows([Row | Rest]) :-
     display_board_rows(Rest).  % Exibe as linhas restantes
 
 display_row(Row) :-
-    write('│'), write(' '), write('▒'), write('▒'),           % Começa com uma borda lateral
+    put_code(0x2502), write(' '), put_code(0x2592), put_code(0x2592),           % Começa com uma borda lateral
     display_cells(Row),   % Exibe as células da linha
-    write('▒'), write('▒'), write(' '), write('│'), nl.       % Fecha com uma borda lateral e pula linha
+    put_code(0x2592), put_code(0x2592), write(' '), put_code(0x2502), nl.       % Fecha com uma borda lateral e pula linha
 
 display_cells([]) :- !.   % Caso base: nada para exibir
 display_cells([Cell | Rest]) :-
-    put_char(Cell),
+    display_code(Cell),
     display_cells(Rest).     % Continua exibindo as células restantes
+
+display_code('S'):- put_code(0x2588).
+display_code('W'):- put_code(0x2593).
+display_code('B'):- put_code(0x2592).
+
 
 displayPlayer(p1):-
     write('Player 1 move : '), nl.
@@ -143,10 +151,10 @@ piece_from_number(2, piece2).
 piece_from_number(3, piece3).
 piece_from_number(4, piece4).
 
-piece_coordinates(piece1, [['▓','▓','▓','▓'], ['▒','▒','▒','▒']]).
-piece_coordinates(piece2, [['▓','▓','▒','▒'], ['▓','▓','▒','▒']]).
-piece_coordinates(piece3, [['▒','▒','▒','▒'], ['▓','▓','▓','▓']]).
-piece_coordinates(piece4, [['▒','▒','▓','▓'], ['▒','▒','▓','▓']]).
+piece_coordinates(piece1, [['W','W','W','W'], ['B','B','B','B']]).
+piece_coordinates(piece2, [['W','W','B','B'], ['W','W','B','B']]).
+piece_coordinates(piece3, [['B','B','B','B'], ['W','W','W','W']]).
+piece_coordinates(piece4, [['B','B','W','W'], ['B','B','W','W']]).
 
 
 update_piece([Row|Rest], 1, Col, Piece, [NewRow|Rest]):- %Row of alteration
@@ -220,15 +228,5 @@ move([Player, Board, OtherPlayer], [Piece, Y, X], [OtherPlayer, Board8, Player])
     update_piece(Board6, NewY+1, NewX+2, V6, Board7),
     update_piece(Board7, NewY+1, NewX+3, V7, Board8).
 
-
-
-% Progress
-print_title :-
-    writeln('██████╗ ██╗     ██╗███╗   ██╗ ██████╗ '),
-    writeln('██╔══██╗██║     ██║████╗  ██║██╔═══██╗'),
-    writeln('██████╔╝██║     ██║██╔██╗ ██║██║   ██║'),
-    writeln('██╔══██╗██║     ██║██║╚██╗██║██║▄▄ ██║'),
-    writeln('██████╔╝███████╗██║██║ ╚████║╚██████╔╝'),
-    writeln('╚═════╝ ╚══════╝╚═╝╚═╝  ╚═══╝ ╚══▀▀═╝').
 
 
