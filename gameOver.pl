9d4acc6db0ea4901b470693c0e6adae7533a12c3
Alexandre Costa <costinhacosta2004@gmail.com>
2024-12-28 00:52:18 +0000

    finalmente está como quero

--- gameOver.pl
+++ gameOver.pl
@@ -5,11 +5,9 @@ process_line(_, _, [], _, _,_,false).
 
 process_line(Board, Y, [PlayerColor|Rest], X, PlayerColor, Visited, Success) :-
     NextX is X+1,
-    is_valid_cell(Board, [X,Y], Color), % Verifica célula antes de chamar dfs
+    is_valid_cell(Board, [X,Y], Color), 
     \+ member([X, Y], Visited),
-    write([X,Y]), write(Visited), write('Bef'),
     dfs(Board, [X, Y], Visited, PlayerColor, NewVisited, DfsSuccess),
-    write(NewVisited), write('Aft'),nl,
     ( DfsSuccess = true -> Success = true ; process_line(Board, Y, Rest, NextX, PlayerColor, NewVisited, Success) ).
 
 process_line(Board, Y, [_|Rest], X, PlayerColor, Visited, Success) :-
@@ -57,45 +55,39 @@ if_valid_dfs(Board, [Col, Row], VisitedIn, Color, VisitedOut, Success) :-
 
 if_valid_dfs(_, _, VisitedIn, _, VisitedIn, false).
 
-% dfs principal
+
 dfs(Board, [Col, Row], VisitedIn, Color, VisitedOut, Success) :-
-    \+ member([Col, Row], VisitedIn),       % Garante que a célula não foi visitada antes
-    NewVisited = [[Col, Row] | VisitedIn], % Atualiza a lista de visitados
-    write('#'), write(NewVisited), write('#'),
-    % Tenta explorar direções uma por vez  
-    (   
-        write('Going down'), nl,
-        NewRow1 is Row - 1,
-        if_valid_dfs(Board, [Col, NewRow1], NewVisited, Color, VisitedOut, Success),
-        Success = true
-    ;
-        write('Going up'), nl,
-        NewRow2 is Row + 1,
-        if_valid_dfs(Board, [Col, NewRow2], NewVisited, Color, VisitedOut, Success),
-        Success = true
-    ;
-        write('Going left'), nl,
-        NewCol1 is Col - 1,
-        if_valid_dfs(Board, [NewCol1, Row], NewVisited, Color, VisitedOut, Success),
-        Success = true
-    ;
-        write('Going right'), nl,
-        NewCol2 is Col + 1,
-        if_valid_dfs(Board, [NewCol2, Row], NewVisited, Color, VisitedOut, Success),
-        Success = true
-    ;
-        % Se todas as direções falharem, define como falha
-        write('All directions failed for '), write([Col, Row]), nl,
-        VisitedOut = NewVisited,
-        Success = false
-    ).
+    \+ member([Col, Row], VisitedIn),       
+    NewVisited = [[Col, Row] | VisitedIn],   
+
+    NewRow1 is Row - 1,
+    if_valid_dfs(Board, [Col, NewRow1], NewVisited, Color, VisitedOutDown, SuccessDown),
+    (SuccessDown -> Success = true, ! ;   
+
+    NewRow2 is Row + 1,
+    if_valid_dfs(Board, [Col, NewRow2], VisitedOutDown, Color, VisitedOutUp, SuccessUp),
+    (SuccessUp -> Success = true, ! ;
+
+    NewCol1 is Col - 1,
+    if_valid_dfs(Board, [NewCol1, Row], VisitedOutUp, Color, VisitedOutLeft, SuccessLeft),
+    (SuccessLeft -> Success = true, ! ;
+
+    NewCol2 is Col + 1,
+    if_valid_dfs(Board, [NewCol2, Row], VisitedOutLeft, Color, VisitedOut, SuccessRight),
+    (SuccessRight -> Success = true, ! ;
+
+    Success = false
+    )))).
+
+
+
 
 
         
 
 
 
-game_over([Player, [FirstLine|Board], Levels, OtherPlayer, 24],  'T'). % Maybe we need to check for win 1 last time here
+game_over([Player, [FirstLine|Board], Levels, OtherPlayer, 0],  'T').
 game_over([Player, [FirstLine|Board], Levels, OtherPlayer, MovesPlayed], Winner) :-
     process_line([FirstLine|Board], 10, FirstLine, 1, 'W',[], WhiteWins),
     process_column([FirstLine|Board], 1, [FirstLine|Board], 1, 'B', [], BlackWins),
